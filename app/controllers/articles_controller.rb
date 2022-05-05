1162e548a41776052a7b06ccc28a54b29cbdd138
Zach Lefler <leaving.valhalla@gmail.com>
2022-05-04 19:13:07 -0700

    commit tree 2ff9876ab9b262be8cd21a603c829bb2301a7876

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,14 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    if session[:page_views] <= 2
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: {error: 'Maximum pageview limit reached'}, status: 401
+    end
+    session[:page_views] += 1
   end
 
   private
